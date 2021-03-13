9a3d9860020b7edc9466a512b8f9a9abcc5521c6
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-13 13:31:33 +0100

    cd..

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import numpy as np
 from numpySpeed import speedNumpy
 
 A = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-print(A[5:10])
+#print(A[5:10])
 
-print(A[-2:])
-print(A[0:-2])
+#print(A[-2:])
+#print(A[0:-2])
 
-speedNumpy()
+# speedNumpy()
 
+B = np.reshape(np.arange(0, 25), [5, -1])
+
+print(B)
+print("*****************************")
+
+print(B[0:3,:])
+
+#print(f"shape(B): {np.shape(B)}")
