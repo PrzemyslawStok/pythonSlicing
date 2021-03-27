5bca95ed9228499e68e47c92a2e8a6a136034267
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-27 12:19:58 +0100

    cd..

--- plot1d.py
+++ plot1d.py
@@ -1,8 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plot
 
-x: np.ndarray = np.linspace(-10, 10, 5)
+x: np.ndarray = np.linspace(-10, 10, 100)
+y: np.ndarray = np.sin(x)
+
 print(x)
 
-plot.plot(x,x)
+plot.plot(x,y)
 plot.show()
