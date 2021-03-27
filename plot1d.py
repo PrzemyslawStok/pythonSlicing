51493879f8ffd4e168156b474ff6106c663a1f75
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-27 12:24:07 +0100

    cd..

--- plot1d.py
+++ plot1d.py
@@ -1,6 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plot
 
+def plotFunction(x: np.ndarray, y: np.ndarray):
+    plot.plot(x, y)
+    plot.show()
+
+
 x: np.ndarray = np.linspace(-10, 10, 100)
 y: np.ndarray = np.sin(x)
 
@@ -8,3 +13,12 @@ print(x)
 
 plot.plot(x,y)
 plot.show()
+
+plot.plot(x,y+x)
+plot.show()
+
+for i in range(5):
+    pass
+
+#proszę narysować obrazki dla x, x^2 ... x^5
+
