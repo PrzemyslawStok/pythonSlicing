169b8c49943657b5b35c08672256da633652f5c5
= <=>
2021-04-10 13:18:36 +0200

    cd..

--- speedPython.py
+++ speedPython.py
@@ -1,7 +1,7 @@
 import timeit
 
 import numpy as np
-from numba import jit, njit
+from numba import jit, njit, prange
 
 def function0():
     A = np.arange(0, 1000_000)
@@ -25,6 +25,14 @@ def function3():
     A = np.arange(0, 1000_000)
     A = A**2
 
+@njit(parallel=True)
+def function_parallel():
+    A = np.arange(0, 1000_000)
+
+    for i in prange(len(A)):
+        A[i] = A[i] * A[i]
+
+
 def functionSpeed(function, name: str, run_number = 10):
     for i in range(run_number):
         startTime = timeit.default_timer()
@@ -36,5 +44,10 @@ functionSpeed(function0,"simple run",1)
 functionSpeed(function1,"numba run",5)
 functionSpeed(function2,"numpy run",5)
 functionSpeed(function3,"numpy compile run",5)
+functionSpeed(function_parallel,"parallel numba run",5)
+
+print("---------------------------")
+
+function_parallel.parallel_diagnostics(level=4)
 
 #function1.inspect_types()
