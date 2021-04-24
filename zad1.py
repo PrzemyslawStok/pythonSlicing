74181015972fd55bc750cc445931479d1a6072dc
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-04-24 13:18:56 +0200

    cd..

--- zad1.py
+++ zad1.py
@@ -6,19 +6,19 @@ import os, psutil
 
 A: np.ndarray = np.empty([10,10],dtype=np.ndarray)
 
+#do tej macierzy przyporządkujemy macierze losowe o rozmiarach 100x100
+
 for i in range(10):
     for j in range(10):
         A[i,j] = 1
 
 print(A)
 
+#proszę zobaczyć ile program zajmuje w pamięci
+
 process = psutil.Process(os.getpid())
 print(f"Rozmiar pamięci: {process.memory_info().rss}B")
 
-
-#do tej macierzy przyporządkujemy macierze losowe o rozmiarach 100x100
-
-#proszę zobaczyć ile program zajmuje w pamięci
 #proszę zminić typ macierzy losowej na uint8 i zobaczyć co dzieje się z pamięcią
 
 #np.random.random([5,5], dtype="uint8")
