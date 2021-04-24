6d736fbe22d0499d05c5e21383ea3c20cb3e11a3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-04-24 13:18:15 +0200

    cd..

--- zad1.py
+++ zad1.py
@@ -1,6 +1,21 @@
 import numpy as np
+import os, psutil
+
 
 #tworzymy macierz 2d o rozmiarach 10x10
+
+A: np.ndarray = np.empty([10,10],dtype=np.ndarray)
+
+for i in range(10):
+    for j in range(10):
+        A[i,j] = 1
+
+print(A)
+
+process = psutil.Process(os.getpid())
+print(f"Rozmiar pamięci: {process.memory_info().rss}B")
+
+
 #do tej macierzy przyporządkujemy macierze losowe o rozmiarach 100x100
 
 #proszę zobaczyć ile program zajmuje w pamięci
