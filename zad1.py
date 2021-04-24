8373d666ef0e177c2d0422d2058c5cc7ed6eb7a7
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-04-24 13:31:05 +0200

    cd..

--- zad1.py
+++ zad1.py
@@ -4,20 +4,22 @@ import os, psutil
 
 #tworzymy macierz 2d o rozmiarach 10x10
 
-A: np.ndarray = np.empty([10,10],dtype=np.ndarray)
+A: np.ndarray = np.empty([100,100],dtype=np.ndarray)
 
 #do tej macierzy przyporządkujemy macierze losowe o rozmiarach 100x100
 
 for i in range(10):
     for j in range(10):
-        A[i,j] = 1
+        A1: np.ndarray = np.random.random([1000,1000])
+        A[i,j] = A1.astype(dtype="float64")
 
 print(A)
 
 #proszę zobaczyć ile program zajmuje w pamięci
 
 process = psutil.Process(os.getpid())
-print(f"Rozmiar pamięci: {process.memory_info().rss}B")
+print(f"Rozmiar pamięci: {process.memory_info()}")
+print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
 
 #proszę zminić typ macierzy losowej na uint8 i zobaczyć co dzieje się z pamięcią
 
