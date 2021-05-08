050ab08ca1270184554a3255b7867d30081c9ca5
= <=>
2021-05-08 12:58:41 +0200

    cd

--- zad2.py
+++ zad2.py
@@ -1,4 +1,17 @@
+import numpy as np
+import os, psutil
+
 #Zad.1
 
 #proszę utowrzyć macierz o rozmiarze 1000x1000 i wypełnić ją libami od 0 do 99999
-#proszę sprawdzić rozmiar macierzy dla typów float64 i uint16
+#proszę sprawdzić rozmiar macierzy dla typów float64 i uint32
+
+A = np.arange(0,1000000,dtype="uint32")
+print(A)
+B = np.reshape(A,[1000,-1])
+print(f"Maksymalna liczba: {np.max(A)}")
+print(B)
+
+
+
+#proszę sprawdzić ile zajmuje w pamięci macierz B
