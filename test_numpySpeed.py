b0cf98519161c18066db3c38d6797b3c4b27a3c3
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-05-29 12:40:06 +0200

    cd..

---
+++ test_numpySpeed.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_compile_speed(self):
+        self.assertTrue(True)
+
+    def test_function0(self):
+        self.assertTrue(False)
