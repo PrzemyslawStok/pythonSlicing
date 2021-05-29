2a6c9092e32e5fa61f1629bf2c5e1b4ae517f92f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-05-29 13:10:17 +0200

    cd..

--- test_numpySpeed.py
+++ test_numpySpeed.py
@@ -5,15 +5,15 @@ import timeit
 
 
 class Test(TestCase):
-    def test_compile_speed(self):
+    def test_speed_numpy(self):
         startTime = timeit.default_timer()
         speedNumpy(1000_000, showText=False)
         endTime = timeit.default_timer()
 
-        if endTime-startTime<0.21693:
+        if endTime - startTime < 0.21693:
             self.assertTrue(True)
         else:
             self.fail(f"Funkcja nie jest wystarczająco szybka, czas działania: {(endTime - startTime):0.5f}s")
 
-    def test_function0(self):
+    def test_compile_speed(self):
         self.assertTrue(True)
