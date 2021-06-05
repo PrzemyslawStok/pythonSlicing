1a47cdfc6a43e7a227e9892235f8382db49d557f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-05 12:32:53 +0200

    cd..

--- python3d.py
+++ python3d.py
@@ -1,2 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plot
+
+print("Plot 3d")
+
+x: np.ndarray = np.linspace(-1.0, 1.0, 100)
+y: np.ndarray = np.sin(x)
+
+plot.plot(x,y)
+plot.show()
