2475d86c6c7e491b94448918fc0f73d06351e19c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-05 13:08:12 +0200

    cd..

--- python3d.py
+++ python3d.py
@@ -3,8 +3,18 @@ from matplotlib import pyplot as plot
 
 print("Plot 3d")
 
-x: np.ndarray = np.linspace(-1.0, 1.0, 100)
+x: np.ndarray = np.linspace(-1.0, 1.0, 200)
 y: np.ndarray = np.sin(x)
 
-plot.plot(x,y)
+figure = plot.figure(figsize=(10,10))
+
+for i in range(25):
+    plot.subplot(5,5,i+1)
+    plot.plot(x,x**i)
+
+plot.show()
+
+figure = plot.figure(figsize=(5,5))
+figure.add_subplot('111',projection='3d')
+
 plot.show()
