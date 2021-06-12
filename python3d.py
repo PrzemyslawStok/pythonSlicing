eee4159bb1008393e9dbd963797e5e62f1cb0f95
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-12 13:30:37 +0200

    cd..

--- python3d.py
+++ python3d.py
@@ -1,20 +1,62 @@
 import numpy as np
 from matplotlib import pyplot as plot
+from mpl_toolkits.mplot3d import Axes3D
 
-print("Plot 3d")
+print("Plot multiplot 2d")
 
 x: np.ndarray = np.linspace(-1.0, 1.0, 200)
 y: np.ndarray = np.sin(x)
 
-figure = plot.figure(figsize=(10,10))
+figure = plot.figure(figsize=(10, 10))
 
 for i in range(25):
-    plot.subplot(5,5,i+1)
-    plot.plot(x,x**i)
+    plot.subplot(5, 5, i + 1)
+    plot.plot(x, x ** i)
 
 plot.show()
 
-figure = plot.figure(figsize=(5,5))
-figure.add_subplot('111',projection='3d')
+print("Plot 3d - funkcja jednowymiarowa")
+
+figure: plot.Figure = plot.figure(figsize=(5, 5))
+axes: Axes3D = figure.add_subplot(111, projection='3d')
+
+x: np.ndarray = np.linspace(0, 1.0, 2)
+y: np.ndarray = np.linspace(0, 1.0, 2)
+
+z = x**2+y
+#plot.plot(x,y,z)
+
+noPoints = 1000
+
+def f1(noPoints):
+    X = np.random.random(noPoints)
+    Y = np.random.random(noPoints)
+    Z = np.random.random(noPoints)
+    return X,Y,Z
+
+X,Y,Z = f1(100)
+
+#axes.scatter(X,Y,Z)
+
+#Zad.1 Proszę wygenerować trzy macierze z losowymi punktami w przestrzeni x,y,z
+#Proszę utworzyć funkcję, która zwraca trzy macierze jednocześnie
+
+plot.show()
+
+figure: plot.Figure = plot.figure(figsize=(5, 5))
+axes: Axes3D = figure.add_subplot(111, projection='3d')
+
+x: np.ndarray = np.linspace(-10, 10, 1000)
+y: np.ndarray = np.linspace(-10, 10, 1000)
+
+print(x)
+print(y)
+
+X,Y = np.meshgrid(x,y)
+Z = np.sin(np.sqrt(X*Y))
+
+#axes.scatter(X,Y,Z)
+
+axes.plot_surface(X,Y,Z,color='b')
 
 plot.show()
