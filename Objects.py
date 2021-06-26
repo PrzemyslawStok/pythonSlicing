5dc0a9a83bbbd0e6ad14572af1564e3af1392891
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-26 12:33:17 +0200

    cd..

---
+++ Objects.py
@@ -0,0 +1,6 @@
+from student import student as Student
+
+student1: Student = Student("Przemysław","Stokłosa")
+student1.print()
+
+Student.printId()
--- student.py
+++ student.py
@@ -1,12 +1,13 @@
 class student:
     name: str
     surname: str
-    def __init__(self,name,surname):
+
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
 
     def print(self):
-        print(f"name {self.name}")
+        print(f"name {self.name}, surname {self.surname}")
 
     @staticmethod
     def printId(self):
