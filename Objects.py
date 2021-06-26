772e7b1d9faf028802c72abea79f1d4d354c0aca
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-26 13:06:59 +0200

    cd..

--- Objects.py
+++ Objects.py
@@ -1,19 +1,20 @@
 from student import student as Student
+from student import students_repository as srepo
+import numpy as np
 
-student1: Student = Student("Przemysław","Stokłosa")
+student1: Student = Student("Przemysław", "Stokłosa")
 student1.print()
 
 Student.printId()
-
 students: list = []
 
 n = 10
 
 for i in range(n):
-    students.append(Student("Przemysław",f"Stokłosa_{i}"))
+    students.append(Student("Przemysław", f"Stokłosa_{i}"))
 
 for student in students:
     student.print()
 
-#dodajemy n studentów z numerowanymi nazwiskami
-#wypisujemy ich pętlą
+# dodajemy n studentów z numerowanymi nazwiskami
+# wypisujemy ich pętlą
--- student.py
+++ student.py
@@ -12,3 +12,15 @@ class student:
     @staticmethod
     def printId():
         print(10.0)
+
+class students_repository:
+    students_array: list
+    def __init__(self):
+        pass
+    def addStudent(array: list, students: student):
+        pass
+    def addStudents(array: list, students: list):
+        pass
+    def printStudents(self):
+        pass
+    pass
