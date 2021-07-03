1e5e3d8a462c9ce4877f8631b214b5f7950e6b25
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-07-03 13:28:40 +0200

    cd..

--- student.py
+++ student.py
@@ -9,13 +9,17 @@ class person:
     def print(self):
         print(f"name {self.name}, surname {self.surname}")
 
+    @staticmethod
+    def printId():
+        print(10.0)
+
 class student(person):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
     @staticmethod
     def printId():
-        print(10.0)
+        person.printId()
 
 class students_repository:
     students_array: list
